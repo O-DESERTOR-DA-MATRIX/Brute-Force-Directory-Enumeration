a334bff57698e69ef20478211b7cd4d77f6efa70
Lucas Oliver ITP-DF <51382097+O-DESERTOR-DA-MATRIX@users.noreply.github.com>
2024-10-01 23:55:40 +0100

    Update Directories_enum.py
    Update 01 oct 24

--- Directories_enum.py
+++ Directories_enum.py
@@ -1,38 +1,52 @@
 import requests
+import sys
+import pyfiglet
 
+# Função para exibir o texto e o símbolo do pinguim
+def display_intro():
+    # Gera o texto "S4V10R" em formato de arte
+    result = pyfiglet.figlet_format("S4V10R")
+    print(result)
 
-def checkurl(ip, wordlist): 
+    # Adiciona o símbolo do pinguim
+    print(r"""
+            .--.       
+           |o_o |      
+           |:_/ |      
+          //   \ \     
+         (|     | )    
+        /'\_   _/`\   
+        \___)=(___/   
+    """)
+    print("\n")
+
+# Função para verificar URLs
+def checkurl(ip, wordlist):
     try:
-        # Open the wordlist file
         with open(wordlist, 'r') as file:
             reading = file.read()
-            spliting = reading.split()  # Split the content of the wordlist by spaces
+            splitting = reading.split()
             
-            # Iterate through each directory/file from the wordlist
-            for objectss in spliting:
-                
-               
-                
+            for i in splitting:
+                url = f"https://{ip}/{i}"
                 try:
-                    url = f"https://{ip}/{objectss}"  # Construct the URL
-                    r = requests.get(url, timeout=2)  # Timeout added to avoid hanging
+                    r = requests.get(url, timeout=1.5)
                     
                     if r.status_code == 200:
                         print(f"URL found: {url}")
                     elif r.status_code == 403:
-                        print(f"URL forbidden: {url}")
-                    
-                except:
-                    pass
-            
+                        print(f"Forbidden URL: {url} (403 code error)")
+
+                except requests.exceptions.RequestException as e:
+                    print(f"Error accessing {url}: {e}")
+                
     except FileNotFoundError:
-        print("Ip Address or Wordlist path not found, verify your Ip Address or your wordlist path and try again.")
-        
+        print("Wordlist path is invalid. Check your input and try again.")           
+
 if __name__ == "__main__":
-    # Ask for IP and wordlist path
-    ip = input("Type your IP or domain: ")
+    display_intro()  # Exibir introdução com "S4V10R" e o pinguim
+    
+    ip = input("Type your URL or IP (without http/https): ")
     wordlist = input("Type your wordlist path: ")
     
-    # Call the URL checking function
     checkurl(ip, wordlist)
-
